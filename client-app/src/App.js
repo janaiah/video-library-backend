887e71780343a4b54b2c87f1015eaf215cd1fc20
PranavtejVanguri <Pranavtej.vanguri@outlook.com>
2025-02-02 22:59:59 -0500

    client app added

---
+++ client-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="body-background">
+      <div className='bg-shade'>
+      <h1 className='text-center text-white  pt-3'>Technologies Video Library</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- routes/video.route.js
+++ routes/video.route.js
@@ -1,10 +1,11 @@
 const router=require("express").Router();
 const {getVideos,getVideoById,addVideo,updateVideo,deleteVideo}=require('../controllers/video.controller.js');
-
+const {verifyToken}=require('../middlewares/authMiddleware.js');
+const verifyRoles=require('../middlewares/roleMiddleware.js');
 router.get("/",getVideos);
 router.get("/:id",getVideoById);
-router.post("/",addVideo);
-router.put("/:id",updateVideo);
-router.delete("/:id",deleteVideo);
+router.post("/",verifyToken,verifyRoles("admin"),addVideo);
+router.put("/:id",verifyToken,verifyRoles("admin"),updateVideo);
+router.delete("/:id",verifyToken,verifyRoles("admin"),deleteVideo);
 
 module.exports=router
